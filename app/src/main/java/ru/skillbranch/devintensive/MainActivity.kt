95dbd769f98a8d0cf6663d35327ba3681a0c971c
Sheik80 <sheik80@gmail.com>
2019-07-12 15:15:08 +0900

    Update MainActivity.kt

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,14 +1,12 @@
 package ru.skillbranch.devintensive
 
-import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("MainActivity", "onCreate")
     }
 }
