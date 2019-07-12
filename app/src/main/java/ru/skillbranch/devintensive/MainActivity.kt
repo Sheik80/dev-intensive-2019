a6d541c4b39f951f9d2af655a27edc97c5c2ca54
Sheik80 <sheik80@gmail.com>
2019-07-12 14:51:27 +0900

    hometask_2 complete

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,7 +1,6 @@
 package ru.skillbranch.devintensive
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
