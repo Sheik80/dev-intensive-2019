5c4fce5bfdd7d08b53a5115250f386c421ff288c
Sheik80 <sheik80@gmail.com>
2019-07-31 17:35:17 +0900

    hometask_3 complete

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -42,16 +42,16 @@ class Bender(var status: Status = Status.NORMAL, var question:Question = Questio
         NAME("Как меня зовут", listOf("бендер", "bender")){
             override fun nextQuestion(): Question = PROFESSION
         },
-        PROFESSION("Назови мою профессию", listOf("сгибальщик","bender")){
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик","bender")){
             override fun nextQuestion(): Question = MATERIAL
         },
-        MATERIAL("Из чего я сделан", listOf("металл","дерево", "metal", "iron", "wood")){
+        MATERIAL("Из чего я сделан?", listOf("металл","дерево", "metal", "iron", "wood")){
             override fun nextQuestion(): Question = BDAY
         },
-        BDAY("Когда меня создали", listOf("2993")){
+        BDAY("Когда меня создали?", listOf("2993")){
             override fun nextQuestion(): Question =SERIAL
         },
-        SERIAL("Мой серийный номер", listOf("2716057")){
+        SERIAL("Мой серийный номер?", listOf("2716057")){
             override fun nextQuestion(): Question = IDLE
         },
         IDLE("На этом все, вопросов больше нет", listOf()){
