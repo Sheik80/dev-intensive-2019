952af765845abe368d5319ffbfcb5f33c0f80317
Sheik80 <sheik80@gmail.com>
2019-10-05 13:30:22 +0900

    hometask_5 complete

---
+++ app/src/main/java/ru/skillbranch/devintensive/ui/main/MainActivity.kt
@@ -0,0 +1,29 @@
+package ru.skillbranch.devintensive.ui.main
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ru.skillbranch.devintensive.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initToolbar()
+        initViews()
+        initViewModel()
+
+    }
+
+    private fun initViewModel() {
+
+    }
+
+    private fun initViews() {
+
+    }
+
+    private fun initToolbar() {
+
+    }
+}
